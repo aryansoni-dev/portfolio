17d3deae4f4f849089ae34fd9400480a3dcc2c3e
aryansoni-dev <aryan23112004@gmail.com>
2026-07-14 16:34:47 +0530

    [+] Updated the projects section.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -132,7 +132,7 @@ export const ProjectsSection = () => {
 				<SectionHeader title="Real-world Results" eyebrow="Featured Projects" description="See how I transformed concepts into engaging digital experiences." />
 
 				<div className="flex-col flex mt-10 gap-20 md:mt-20">
-					{portfolioProjects.map((project, projectIndex) => (
+					{[...portfolioProjects].reverse().map((project, projectIndex) => (
 						<Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky" style={{ top: `calc(64px + ${projectIndex * 40}px)` }} >
 							<div className="absolute inset-0 -z-10 opacity-5" style={{ backgroundImage: `url(${GrainImg.src})` }}></div>
 							<div className=" lg:grid lg:grid-cols-2 lg:gap-16 ">
